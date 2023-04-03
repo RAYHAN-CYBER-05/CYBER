afc293ed61837942e7d109f5963ecb26524fabf5
RAYHAN-SHAKIB <129272834+RAYHAN-CYBER-05@users.noreply.github.com>
2023-04-04 01:37:57 +0600

    Add files via upload

---
+++ Update .py
@@ -0,0 +1,21 @@
+:authority: 
+free.facebook.com
+:method: 
+:method: GET
+/
+:path: 
+:scheme: 
+https
+text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+accept-encoding: 
+gzip, deflate, br
+accept-language: 
+en-US,en;q=0.9
+cache-control: 
+max-age=0
+datr=CqcqZDUkZts3K2_V0VXqtDnt; sb=CqcqZJ9yFC4JuO7qKOFsy89Y; fr=0AD8iodvv7O39JXaX..BkKqcK.V-.AAA.0.0.BkKqcn.AWWOvPiDm8Y
+sec-ch-ua: 
+sec-ch-ua: "Chromium";v="111", "Not(A:Brand";v="8"
+sec-ch-ua-mobile: 
+sec-ch-ua-platform: 
+sec-ch-ua-platform: "Android"
